TheDuriel/MagicMacros

commit 86b654b6e6e7b6a1551c84681d585ac7af969125

Changes (#9)
* Update

.

* Removed personal comments

* minor changes

* .

* .

* .

Components/InputCatcher.gd
@@ -5,15 +5,13 @@ extends Node
 # One of these gets attached to every engine window unless it already has one.
 # Yeah that's fucked.
 
-signal tab_pressed(window: Window)
-
+signal tab_pressed
 var _window: Window
 
 
-func _init(window: Window) -> void:
-	_window = window
-	_window.add_child(self)
 
+func _init()->void:
+	EditorInterface.get_script_editor().add_child(self)
 
 func _input(event: InputEvent) -> void:
 	if not event is InputEventKey:
@@ -25,4 +23,4 @@ func _input(event: InputEvent) -> void:
 		return
 	
 	if e.pressed and not e.is_echo() and not e.is_released():
-		tab_pressed.emit(_window)
+		tab_pressed.emit()

MagicMacros.gd
@@ -45,15 +45,13 @@ var _current_line_data: MagicMacrosLineData:
 		_current_line_data = value
 		_update_line_color()
 
-var _window_check_time: float = 0.0
-var _window_check_wait_time: float = 5.0
-
-var _input_catchers: Dictionary = {} # window, catcher
-
 
 func _ready() -> void:
+	await get_tree().create_timer(1).timeout
 	_load_macros()
-	EditorInterface.get_script_editor().script_changed.connect(_on_script_changed)
+	EditorInterface.get_script_editor().editor_script_changed.connect(_on_script_changed)
+	var InputCatcher:MagicMacrosInputCatcher = MagicMacrosInputCatcher.new()
+	InputCatcher.tab_pressed.connect(_on_tab_pressed)
 	_current_editor = EditorInterface.get_script_editor().get_current_editor()
 	pascal_case_regex = RegEx.new()
 	pascal_case_regex.compile(PASCAL_CASE_REGEX_PATTERN)
@@ -62,35 +60,12 @@ func _ready() -> void:
 	print("MagicMacros: Enabled")
 
 
+
 func _exit_tree() -> void:
 	if _current_editor:
 		var base: TextEdit = _current_editor.get_base_editor()
 		base.remove_theme_color_override(THEME_COLOR_CONSTANT)
-	for node: Node in _input_catchers.values():
-		node.queue_free()
 
-
-func _process(delta: float) -> void:
-	_window_check_time += delta
-	if _window_check_time > _window_check_wait_time:
-		_window_check_time = 0.0
-		_attach_input_catchers()
-
-
-func _attach_input_catchers() -> void:
-	var windows: Array[Window] = []
-	
-	for window_id: int in DisplayServer.get_window_list():
-		var instance_id: int = DisplayServer.window_get_attached_instance_id(window_id)
-		var window: Window = instance_from_id(instance_id)
-		windows.append(window)
-	
-	for window: Window in windows:
-		
-		if not window in _input_catchers:
-			_input_catchers[window] = MagicMacrosInputCatcher.new(window)
-			_input_catchers[window].tab_pressed.connect(_on_tab_pressed)
-			print("MagicMacros: Window hooked.")
 
 
 func _load_macros() -> void:
@@ -140,8 +115,8 @@ func _update_line_color() -> void:
 		base.remove_theme_color_override(THEME_COLOR_CONSTANT)
 
 
-func _on_tab_pressed(window: Window) -> void:
-	var base: TextEdit = _current_editor.get_base_editor()
+func _on_tab_pressed() -> void:
+	var base: CodeEdit = _current_editor.get_base_editor()
 	if not base:
 		return
 	if not base.is_visible_in_tree() and base.has_focus():
@@ -154,8 +129,8 @@ func _on_tab_pressed(window: Window) -> void:
 	base.set_line(_current_line_data.id, _current_line_data.modified_text)
 	base.set_caret_column(0)
 	base.set_caret_line(_current_line_data.id)
-	window.set_input_as_handled()
-
+	base.cancel_code_completion()
+	EditorInterface.get_script_editor().get_viewport().set_input_as_handled()
 
 func is_pascal_case(string: String) -> bool:
 	return true if pascal_case_regex.search(string) else false

Components/LineData.gd
@@ -22,7 +22,7 @@ var has_identifier: bool:
 	get: return not identifier_args.is_empty()
 
 var identifier: String:
-	get: return identifier[0] if has_identifier else DEFAULT_IDENTIFIER
+	get: return identifier_args[0] if has_identifier else DEFAULT_IDENTIFIER
 
 var type_args: Array[String] = []
 
@@ -66,13 +66,14 @@ func _parse_line() -> void:
 	var identifiers: Array[String] = []
 	var remainders: Array[String] = []
 	
+	if args.size() and _arg_is_identifier(args[0]):
+		identifiers.append(args[0])
+		args.remove_at(0)
+	if args.size():
+		types.append(args[0])
+		args.remove_at(0)
 	for arg: String in args:
-		if _arg_is_type(arg):
-			types.append(arg)
-		elif _arg_is_identifier(arg):
-			identifiers.append(arg)
-		else:
-			remainders.append(arg)
+		remainders.append(arg)
 	
 	type_args = types
 	identifier_args = identifiers
@@ -98,8 +99,7 @@ func _arg_is_identifier(arg: String) -> bool:
 
 func _get_macro_script() -> Script:
 	for macro: Script in _plugin.macros:
-		var matches: bool = macro.call(_plugin.macros_alias_func, source_text)
+		var matches: bool = macro.call(_plugin.macros_alias_func, macro_arg)
 		if matches:
 			return macro
-	
 	return null

Macros/Function.gd
@@ -9,10 +9,13 @@ static func is_macro_alias(arg: String) -> bool:
 
 
 static func apply_macro(line_data: MagicMacrosLineData) -> String:
-	
 	var s: String = ""
 	s += "func %s() -> %s:" % [line_data.identifier, line_data.type]
 	s += "\n"
-	s += "	pass"
-	
+	if line_data.type == line_data.DEFAULT_TYPE:
+		s += "	pass"
+	else:
+		s += "	var value: %s"% [line_data.type]
+		s += "\n"
+		s += "	return value"
 	return s

commit 554dc4ce270043673ffb2d512fc36065d5a872e5

fix tab interception not actually working anymore
this is a regression from 86b654b6e6e7b6a1551c84681d585ac7af969125

Components/InputCatcher.gd
@@ -7,7 +7,7 @@ extends Node
 signal tab_pressed
 
 
-func _init()->void:
+func _init() -> void:
 	EditorInterface.get_script_editor().add_child(self)
 
 

MagicMacros.gd
@@ -144,11 +144,14 @@ func _on_tab_pressed() -> void:
 	if not _current_line_data.is_valid:
 		return
 	
-	base.set_line(_current_line_data.id, _current_line_data.modified_text)
+	_current_editor.get_viewport().set_input_as_handled()
+	base.cancel_code_completion()
+	
+	base.set_line(_current_line_data.line_index, _current_line_data.modified_text)
 	base.set_caret_column(0)
 	base.set_caret_line(_current_line_data.id)
+	
 	base.cancel_code_completion()
-	EditorInterface.get_script_editor().get_viewport().set_input_as_handled()
 
 
 func is_pascal_case(string: String) -> bool:
a8d9c97152d03d116a72b40f66ddf663afad0917
TheDuriel <manueldurielfischer@gmail.com>
2024-11-06 15:46:10 +0100

    clean up input catcher

--- Components/InputCatcher.gd
+++ Components/InputCatcher.gd
@@ -1,17 +1,13 @@
 class_name MagicMacrosInputCatcher
 extends Node
-# Catches inputs in owning window.
-# This is really awkward but the only solution I can see working.
-# One of these gets attached to every engine window unless it already has one.
-# Yeah that's fucked.
+# This class intercepts inputs meant for the CodeEditor
 
 signal tab_pressed
-var _window: Window
-
 
 
 func _init()->void:
 	EditorInterface.get_script_editor().add_child(self)
+
 
 func _input(event: InputEvent) -> void:
 	if not event is InputEventKey:
